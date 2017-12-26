7e9ff130c76653a63cb097d7c4f358c9160d5efa
Subrahmanyam Padala <subrahmanyam.03@gmail.com>
2017-12-26 17:01:16 +0530

    state for toggling details of weather added

--- src/store/index.js
+++ src/store/index.js
@@ -18,19 +18,26 @@ const store = new Vuex.Store({
       commit('REMOVE_CITY', item.city)
     },
     GET_CURRENT_WEATHER: function ({ commit, state }, item) {
-      axios.get(endpoint + 'q=' + item + '&APPID=' + api_key + '')
+      axios.get(endpoint + 'q=' + item + '&units=metric&APPID=' + api_key + '')
         .then(response => {
           commit('SET_CURRENT_WEATHER', response)
         })
         .catch(e => {
           this.errors.push(e)
         })
+    },
+    TOGGLE_DESC: function ({ commit }, item) {
+      commit('TOGGLE_DESC', item)
     }
   },
   mutations: {
     SET_CURRENT_WEATHER: (state, weather_report) => {
-      state.currentWeatherReport = weather_report.data.list.slice(0, 5);
-    
+      state.currentWeatherReport = weather_report.data.list.slice(0, 5).map(item=>{
+         item.show=false;
+        item.icon="http://openweathermap.org/img/w/"+item.weather[0].icon+".png";
+         return item
+       });
+
     },
     REMOVE_CITY: (state, city_name) => {
       state.AllSelectedCities.splice(state.AllSelectedCities.indexOf(city_name), 1);
@@ -38,6 +45,9 @@ const store = new Vuex.Store({
     ADD_CITY: (state, city_name) => {
 
       state.AllSelectedCities.push(city_name)
+    },
+    TOGGLE_DESC: (state,index) => {
+      state.currentWeatherReport[index].show = !state.currentWeatherReport[index].show
     }
   },
   getters: {
